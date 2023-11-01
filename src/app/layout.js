aaf31cc47a8383ad0b6f1dc6d4b1400b1e332b02
Elian Valdez <elian.valdez09@gmail.com>
2023-11-01 19:57:28 +0000

    asdasdasdasd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Nunito } from "next/font/google";
+import "./global.css";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -10,29 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      {/* <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#febf00" />
-        <meta name="msapplication-TileColor" content="#febf00" />
-        <meta name="theme-color" content="#ffffff" />
-      </head> */}
       <body className={nunito.className}>{children}</body>
     </html>
   );
